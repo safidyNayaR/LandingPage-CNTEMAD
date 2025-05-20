38b9a6752018aab9b5e3336183d6b8a2c677777c
safidyNayaR <144090997+safidyNayaR@users.noreply.github.com>
2025-05-20 10:28:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+// ============= RESPONSIVE TYPEWRITER ============== //
+
+const title = document.querySelector(".titre-cntemad");      
+// ============= MAX-MD ===================== //
+if (window.innerWidth < 768) {
+  title.classList.remove("typewriter")
+}
+
+// ============== SHOW MENU ====================== //
+// ============ CONST OF MENU =================== //
+const nav_resp = document.querySelector(".nav_resp"),
+      nav_liste = document.querySelector("#nav_liste"),
+      nav_close = document.querySelector("#nav-close");
+
+// ============== ADD MENU WITH RESP =============== //
+if (nav_resp) {
+  nav_resp.addEventListener('click', () => {
+    nav_liste.classList.add("show_menu")
+  })
+}
+
+// ============== REMOVE MENU IF EXIST ============ //
+if (nav_close) {
+  nav_close.addEventListener('click', () => {
+    nav_liste.classList.remove("show_menu")
+  })
+}
+
+// ============== ACTIVE LINK RESP =============== //
+
+const linkNav = document.querySelectorAll(".link-nav")
+
+function linkActive() {
+  // ================= NAV LIST ================== //
+  document.querySelector("#nav_liste").classList.remove("show_menu");
+}
+linkNav.forEach(link => link.addEventListener('click', linkActive));
